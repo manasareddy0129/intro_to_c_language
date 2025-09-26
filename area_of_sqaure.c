d61aa933de19b62cb79209108fe86a22757b2345
manasareddy0129 <seelammanasareddy3@gmail.com>
2025-09-26 10:31:22 +0530

    area_of_sqaure.c

---
+++ area_of_sqaure.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+int main()
+{
+  int length =2;
+int area;
+area = length * length;
+printf("length of square is %d\n",length);
+printf("area of square is %d\n",area);
+return 0;
+}
