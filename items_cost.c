e49a0d9d736120bb7b24b0aaf6a965930855cab8
manasareddy0129 <seelammanasareddy3@gmail.com>
2025-09-26 11:52:22 +0530

    Create items_cost.c

---
+++ items_cost.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int no_of_items = 30;
+int cost_of_each_item = 20;
+int total_cost_of_items = no_of_items * cost_of_each_item;
+printf("Number of items is %d\n",no_of_items);
+printf("Cost of each item is %d\n",cost_of_each_item);
+printf("cost of all items is %d",total_cost_of_items);
+return 0;
+}
