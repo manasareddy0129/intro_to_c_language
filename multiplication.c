e51872eb43ceca6328ad2d8c4b3dc26a79f2397c
manasareddy0129 <seelammanasareddy3@gmail.com>
2025-09-26 12:16:18 +0530

    Create multiplication.c

---
+++ multiplication.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+  int no_of_jars=12;
+int cost_of_each_jar=23;
+int total_cost_of_jar=no_of_jars*cost_of_each_jar;
+printf("Number of jars is %d\n", no_of_jars);
+printf("each jar cost is %d\n",cost_of_each_jar);
+printf("totam cost of all jars is %d",total_cost_of_jar);
+return 0;
+}
