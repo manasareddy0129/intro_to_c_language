a1c1a0d12f9eea0ff1559aeb34836f824f8edcfa
manasareddy0129 <seelammanasareddy3@gmail.com>
2025-09-26 10:38:48 +0530

    Create area_of_rectangle.c

---
+++ area_of_rectangle.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main()
+{
+  int length = 6;
+int breadth =3;
+int area ;
+
+area = length * breadth ;
+printf("length of rectangle is %d\n",length);
+printf("breadth of rectangle id %d\n",breadth);
+printf("Area of rectangle is %d",area);
+return 0;
+}
