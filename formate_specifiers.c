55e1bfe4c0245a0a3a9f967b5d7c5df9092ce0d5
manasareddy0129 <seelammanasareddy3@gmail.com>
2025-09-26 09:39:01 +0530

    formate_specifiers.c

---
+++ formate_specifiers.c
@@ -0,0 +1,11 @@
+#include<stdio.h>
+int main()
+{
+int myNum=20;
+char myLetter='A';
+float myFloatNum=19.6;
+printf("myNum=%d\n",myNum);
+printf("myLetter=%c\n",myLetter);
+printf("myFloatNum=%f\n",myFloatNum);
+return 0;
+}
