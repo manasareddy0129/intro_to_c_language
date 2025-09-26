e2c8955b6fa875356d363a14e9a0eac1b99d6b54
manasareddy0129 <seelammanasareddy3@gmail.com>
2025-09-26 10:43:06 +0530

    Create perimeter_of_rectangle.c

---
+++ perimeter_of_rectangle.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+int main()
+{
+  int length =7;
+int breadth =3;
+int perimeter ;
+perimeter =2*(length+breadth);
+printf("length of rectangle is %d\n",length);
+printf("Breadth of rectangle is %d\n",breadth);
+printf("perimeter of rectangle id %d",perimeter);
+return 0;
+}
