28542c71d3990b1826d39b9bcf5d1fef20d545a5
manasareddy0129 <seelammanasareddy3@gmail.com>
2025-09-26 20:19:21 +0530

    Create  nestedforloop.c

---
+++ nestedforloop.c
@@ -0,0 +1,16 @@
+#include <stdio.h>
+
+int main() {
+  int i, j;
+
+  for (i = 1; i <= 2; ++i) {
+    printf(" i is increasing: %d\n", i); 
+    
+   
+    for (j = 1; j <= 3; ++j) {
+      printf(" j is  Increasing: %d\n", j); 
+    }
+  }
+  
+  return 0;
+}
