a4bba66ad604fe822599cdec6c9bb2c8d9cb88fd
manasareddy0129 <seelammanasareddy3@gmail.com>
2025-09-26 19:18:42 +0530

    Create do_while3.c

---
+++ do_while3.c
@@ -0,0 +1,16 @@
+
+
+#include <stdio.h>
+​
+int main() {
+  int countdown = 3;
+​
+  while (countdown > 0) {
+    printf("%d\n", countdown);
+    countdown--;
+  }
+​
+  printf("Happy New Year!!\n");
+​
+  return 0;
+}
