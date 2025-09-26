aa0ea3408846d904d9baff3ef0855e72134e322d
manasareddy0129 <seelammanasareddy3@gmail.com>
2025-09-26 10:25:16 +0530

    area_of_square.c

---
+++ area_of_square.c
@@ -0,0 +1,12 @@
+#include<stdio.h>
+
+int main()
+{
+  int side = 8;
+  int perimeter; 
+
+  perimeter = 4 * side; 
+  printf("length of side =%d\n",side);
+  printf("Perimeter of a square=%d",perimeter ); 
+  return 0;
+}
