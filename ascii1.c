21441ff525e7e671315c91c397cd0886bc67f630
manasareddy0129 <seelammanasareddy3@gmail.com>
2025-09-28 12:05:14 +0530

    Create ascii1.c

---
+++ ascii1.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+
+int main() {
+    char ch = 'M';        
+    int asciiValue = ch;  
+
+    printf("Character: %c\n", ch);
+    printf("ASCII Value: %d\n", asciiValue);
+
+    return 0;
+}
