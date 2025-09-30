2805e429334f41a5a79e0be3785b1e86cdadc332
manasareddy0129 <seelammanasareddy3@gmail.com>
2025-09-30 16:12:07 +0530

    Create dupulicate_element_in_array.c

---
+++ dupulicate_element_in_array.c
@@ -0,0 +1,17 @@
+#include<stdio.h>
+int main() {
+    int n;
+    scanf("%d",&n);
+    int a[n];
+    for(int i=0;i<n;i++) scanf("%d",&a[i]);
+    printf("Duplicates: ");
+    for(int i=0;i<n;i++) {
+        for(int j=i+1;j<n;j++) {
+            if(a[i]==a[j]) {
+                printf("%d ", a[i]);
+                break;
+            }
+        }
+    }
+    return 0;
+}
