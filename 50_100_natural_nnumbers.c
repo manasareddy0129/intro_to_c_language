8297989c45404840c1455514138e304369bd7562
manasareddy0129 <seelammanasareddy3@gmail.com>
2025-09-30 12:10:00 +0530

    Create 50_100_natural_nnumbers.c

---
+++ 50_100_natural_nnumbers.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+int main() {
+    int i;
+
+    printf("Natural numbers from 50 to 100 are:\n");
+
+    for(i = 50; i <= 100; i++) {
+        printf("%d ", i);
+    }
+
+    return 0;
+}
